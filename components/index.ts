956887efb689dbf91a74c2fed9190fa35d72b580
ferbautistamollo <ferbautistamollo@gmail.com>
2025-07-25 14:59:22 -0400

    CREACIÓN DE ARCHIVO DE BARRIL

---
+++ components/index.ts
@@ -0,0 +1,15 @@
+export {
+  AffiliateDataIcon,
+  BeneficiariesDataIcon, CancelIcon, ChevronRightIcon, CircleIcon, CollapseFalseIcon,
+  CollapseTrueIcon, DocumentEditIcon, DocumentRegisterIcon, DocumentsDataIcon, DocumentViewIcon, EditIcon, FileDossiersIcon,
+  GarbageIcon, Logo, MoonFilledIcon, MuserpolLogo, PersonalDataIcon, PersonInfoIcon, PersonsIcon, PoliceDataIcon, RegisterIcon, SearchIcon, SunFilledIcon, TouchIcon, TouchIndiceIcon,
+  TouchPulgarIcon, TouchRegisterIcon, VerticalDotsIcon
+} from "./icons";
+
+export { ButtonEdit } from "./buttonEdit";
+export { ButtonRegister } from "./buttonRegister";
+export { CardActions } from "./cardActions";
+export { HeaderManage } from "./headerManage";
+export { ModalAlert } from "./modalAlert";
+export { SpinnerLoading } from "./spinnerLoading";
+export { EmptyContent } from "./emptyContent";
